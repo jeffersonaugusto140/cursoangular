1fc15c5bf86f42ee6ebfb4c4241be4efc2c8521f
Jefferson Augusto da Silva <jefferson.augusto140@gmail.com>
2017-10-13 17:17:18 -0300

    Exec ts

---
+++ exectypescript/app.ts
@@ -0,0 +1,15 @@
+let test: string = "Teste TS......."
+
+console.log(test)
+
+let ep: number = 1
+
+console.log(ep)
+
+ep++
+
+console.log(ep)
+
+let qqercoisa
+qqercoisa = 'asdf ' + 50
+console.log(qqercoisa)
---
+++ exectypescript/dist/app.js
@@ -0,0 +1,9 @@
+var test = "Teste TS.......";
+console.log(test);
+var ep = 1;
+console.log(ep);
+ep++;
+console.log(ep);
+var qqercoisa;
+qqercoisa = 'asdf ' + 50;
+console.log(qqercoisa);
